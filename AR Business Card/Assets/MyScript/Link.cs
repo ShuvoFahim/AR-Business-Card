b1883090697047009f3f9d430bd6cd11cf9201e3
Mohammad Fahim Hossain <fahim.shuvo09@gmail.com>
2019-10-26 23:28:12 +0600

    Link and other stuffs for contact scripts

---
+++ AR Business Card/Assets/MyScript/Link.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void Facebook() {
+
+        Application.OpenURL("tel:+8801876597531");
+
+
+    }
+
+
+
+    public void SendEmail()
+    {
+        string email = "fahim.shuvo09@gmail.com";
+        string subject = MyEscapeURL(" ");
+        string body = MyEscapeURL(" ");
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+    string MyEscapeURL(string url)
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+}
