9d399254b68557577a0626da05812fa3182eb2d5
Mohammad Fahim Hossain <fahim.shuvo09@gmail.com>
2019-11-18 19:47:40 +0600

    Animation, AR virtual Button, more scriptting and so on

---
+++ AR Business Card/Assets/ARButtonController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class ARButtonController : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject ContactButton;
+    public GameObject InformationObject;
+
+    private Animator ContactAnim;
+    private Animator InformationAnim;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] ARButton = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < ARButton.Length; i++) {
+
+            ARButton[i].RegisterEventHandler(this);
+
+        }
+        ContactAnim = ContactButton.GetComponent<Animator>();
+        InformationAnim = InformationObject.GetComponent<Animator>();
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "Introduction")
+        {
+            InformationAnim.SetTrigger("OPEN INFO");
+
+        }
+        else if (vb.VirtualButtonName == "Contact")
+        {
+
+            ContactAnim.SetTrigger("OPEN");
+
+        }
+        else if (vb.VirtualButtonName == "Video") {
+
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- AR Business Card/Assets/_MyScripts/Link.cs
+++ AR Business Card/Assets/_MyScripts/Link.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class Link : MonoBehaviour
 {
-    public void Facebook() {
+    public void Call() {
 
-        Application.OpenURL("tel:+8801876597531");
+        Application.OpenURL("tel:+8801643481347");
 
+
+    }
+
+    public void OpenFacebookProfile() {
+
+
+        Application.OpenURL("fb://profile/1362175752");
+
+
+
+        }
+
+    public void OpenInstaProfile() {
+
+        Application.OpenURL("https://www.instagram.com/pallob_bh");
+
+    }
+
+    public void OpenLinkedinProfile()
+    {
+
+        Application.OpenURL("https://www.linkedin.com/in/a-k-m-bahalul-haque/?originalSubdomain=bd");
 
     }
 
@@ -15,7 +37,7 @@ public class Link : MonoBehaviour
 
     public void SendEmail()
     {
-        string email = "fahim.shuvo09@gmail.com";
+        string email = "bahalul.haque@northsouth.edu";
         string subject = MyEscapeURL(" ");
         string body = MyEscapeURL(" ");
         Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
